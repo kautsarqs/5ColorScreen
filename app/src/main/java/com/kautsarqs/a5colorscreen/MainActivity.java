5de67baced42a38f6099dd5698c9efa78d60905d
kautsarqs <kaustar1777@gmail.com>
2025-03-14 13:53:17 +0700

    Initial commit

---
+++ app/src/main/java/com/kautsarqs/a5colorscreen/MainActivity.java
@@ -0,0 +1,23 @@
+package com.kautsarqs.a5colorscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+    }
+}
